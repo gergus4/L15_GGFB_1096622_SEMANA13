07b1f7f2e4cecba03ae88513b6585fe83f4731e9
gerfu <gerfu@HPGERARDO>
2022-11-07 14:12:13 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("SUMA DE ELEMENTOS POSITIVOS, NEGATIVOS (TABLA 40*50)");
+        int[,] mat = new int[40,50];
+        int numpos = 0;
+        int numneg = 0;
+
+        Random random = new Random();
+        for (int i = 0; i < 40; i++)
+        {
+            for (int j = 0; j < 50; j++)
+            {
+                mat[i,j] = random.Next(-1000,1000);
+            }
+        }
+
+        for (int i = 0; i <40; i++)
+        {
+            for (int j = 0; j < 50; j++)
+            {
+                if (mat[i,j] >= 0)
+                {
+                    numpos = numpos + mat[i,j];
+                }
+
+                else
+                {
+                    numneg = numneg + mat[i,j];
+                }
+            }
+        }
+
+        Console.WriteLine("La suma de los numeros que son positivos es: " + numpos);
+        Console.WriteLine("La suma de los numeros que son negativos es: " + numneg);
+        Console.WriteLine("GRACIAS POR USAR EL PROGRAMA");
+
+    }
+}
